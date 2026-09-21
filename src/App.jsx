bfd1f2f99d459a67c8093245ef5328953a23f86d
FailHy <hylmikhalil@gmail.com>
2026-09-21 12:52:01 +0700

    chore: initialize vite react project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Inventaris App</h1>
+      <p>Setup berhasil. Siap dikembangkan.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,43 @@
+import axiosClient from '../api/axiosClient';
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({children}) {
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(localStorage.getItem('token'));
+
+    const login = async (email, password) => {
+        const response = await axiosClient.post('/login', {email, password});
+
+        const receivedToken = response.data.token;
+        const receivedUser = response.data.user;
+
+        localStorage.setItem('token', receivedToken);
+        setToken(receivedToken);
+        setUser(receivedUser);
+    }
+
+    const logout = async()=> {
+        await axiosClient.post('/logout', {}, {
+            headers: {Authorization: 
+                `Bearer ${token}`
+            },
+        });
+
+        localStorage.removeItem('token');
+        setToken(null);
+        setUser(null);
+    }
+
+    return (
+    <AuthContext.Provider value={{user, token, login, logout}}>
+    {children}
+    </AuthContext.Provider>
+    );
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
