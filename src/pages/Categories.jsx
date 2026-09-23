b72776c5670d1f35b941230e5736551323cdeca3
FailHy <hylmikhalil@gmail.com>
2026-09-23 15:00:04 +0700

    feat(rabu): implement full CRUD for Categories and Items

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/set-state-in-effect */
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import axiosClient from "../api/axiosClient";
